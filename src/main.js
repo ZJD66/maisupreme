e5c524cd5faddd60e38abc433f12a5d53f085667
陈冠希 <zhangjun887@vip.qq.com>
2018-07-25 19:36:25 +0800

    初始项目搭钩完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import './assets/statics/site/css/style.css'
+
+
+//路由中间件
+// Vue.use(VueRouter)
+
+// const router = new VueRouter({
+//   routes: [{
+//       path: '/news',
+//       component: news
+//     },
+//     {
+//       path: '/stars',
+//       component: stars
+//     }
+//   ]
+// })
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  el:'#app',
   render: h => h(App)
-}).$mount('#app')
+  //挂载路由规则
+  // router
+})
